31fcc554cd8b7ca8750df7e7848e6fcb5c455630
Tamkunath <69881782+Tamkunath@users.noreply.github.com>
2020-12-21 19:19:01 -0500

    Add files via upload
    First GitHub Code

---
+++ gitdemo.java
@@ -0,0 +1,12 @@
+public class FirstProgram {
+
+	public static void main (String[] args) {
+		
+		System.out.println("This is my GitHub Demo Program");
+		
+		
+		//System.out.println("This is my Program Ending");
+
+	}
+
+}
